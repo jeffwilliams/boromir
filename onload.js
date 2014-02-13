180b1cca0e4f2b0a1683470b741a04ea9b1faa2e
Austin Stanley <astanley@cnbtxk.com>
2014-02-13 15:58:43 -0600

    moved jscript to seperate files.

---
+++ onload.js
@@ -0,0 +1,89 @@
+onresize = scrollToBottom;
+
+// Prevent touch-based scrolling.
+ontouchstart = function(event) { event.preventDefault(); };
+
+onload = function() {
+  var boromir = Combat.Creature({
+    name: "Gimli",
+    gender: "male",
+    isUnique: true,
+    level: 15,
+    str: 17,
+    dex: 10,
+    con: 17,
+    hp: 128, // 15d10 + 83 Con
+    parts: Boromir.WORDS.humanoidParts,
+    stumbles: Boromir.WORDS.stumbles
+  });
+
+  boromir.equipWeapon(Boromir.utils.makeWeapon("greataxe"));
+  boromir.equipArmor(Boromir.utils.makeArmor("chainmail"));
+
+  function makeOrc() {
+    var orc = Combat.Creature({
+      name: "orc",
+      gender: "male",
+      isUnique: false,
+      level: 4,
+      str: 15,
+      dex: 12,
+      con: 13,
+      hp: 30,
+      parts: Boromir.WORDS.humanoidParts,
+      stumbles: Boromir.WORDS.stumbles
+    });
+
+    orc.equipWeapon(Boromir.utils.makeWeapon("random"));
+    orc.equipArmor(Boromir.utils.makeArmor("cloth"));
+    return orc;
+  }
+  
+  var output = Output(boromir);
+  var view = Grammar.View();
+  var narrator = Combat.Narrator(view, output);
+  var attack = Combat.MeleeAttack(narrator);
+
+  function combat(p1, p2) {
+    var m = [[p1, p2], [p2, p1]];
+    if (p2.check('dex') >= p1.check('dex'))
+      m.reverse();
+    
+    output.emit("begin", p1.definiteName + " and " + p2.definiteName +
+                " close in and begin to fight!");
+    output.pause(2);
+
+    function statusStr(creature) {
+      return creature.name + ": " + creature.hp + "/" + creature.maxHp() +
+             " HP";
+    }
+
+    while (1) {
+      output.emit("status", statusStr(p2) + "  " + statusStr(p1));
+      for (var i = 0; i < m.length; i++) {
+        var attacker = m[i][0];
+        var defender = m[i][1];
+        if (attacker.hp)
+          attack.executeTurn(attacker, defender);
+        if (defender.hp == 0) {
+          output.emit("death", defender.definiteName + " has been killed!");
+          return;
+        }
+        output.pause(3);
+      }
+    }
+  }
+
+  for (var count = 0; boromir.hp > 0; count++) {
+    var orc = makeOrc();
+    output.emit("intro", orc.indefiniteName + " wielding " +
+                orc.weapon.indefiniteName + " approaches!");
+    combat(orc, boromir);
+    output.pause(3);
+  }
+
+  output.emit("conclusion", "After killing " + (count-1) + " " +
+              orc.name + "s, " + boromir.name + " died.");
+
+  output.playback();
+};
---
+++ utils.js
@@ -0,0 +1,67 @@
+function scrollToBottom() {
+  var ODD_EXTRA_SPACE = 16;
+  var windowHeight = window.innerHeight ||
+                     document.documentElement.offsetHeight;
+  var docHeight = document.body.clientHeight;
+  if (windowHeight < docHeight) {
+    var surplus = docHeight - windowHeight + ODD_EXTRA_SPACE;
+    document.body.style.top = "-" +  surplus + "px";
+  }
+}
+
+function Output(hero) {
+  var queue = [];
+
+  function showMessage(message) {
+    var p = document.createElement("p");
+    var baseClass = "combat " + message.category;
+    
+    if (message.attacker)
+      baseClass += " " + message.attacker;
+    p.setAttribute("class", baseClass);
+    p.appendChild(document.createTextNode(message.text));
+    document.body.appendChild(p);
+    setTimeout(function() {
+      p.setAttribute("class", baseClass + " transition");
+    }, 1);
+    scrollToBottom();
+  }
+  
+  function processNextCommand() {
+    if (queue.length) {
+      var command = queue.pop();
+      if (command.type == "pause") {
+        setTimeout(processNextCommand, command.seconds * 1000);
+      } else {
+        showMessage(command);
+        processNextCommand();
+      }
+    }
+  }
+
+  var self = {
+    playback: function() {
+      processNextCommand();
+    },
+    emit: function(category, msg, info) {
+      var attacker = null;
+      if (info && info.attacker) {
+        if (info.attacker === hero)
+          attacker = "hero";
+        else
+          attacker = "villain";
+      }
+      queue.splice(0, 0, {
+        type: "message",
+        category: category,
+        text: msg[0].toUpperCase() + msg.slice(1),
+        attacker: attacker
+      });
+    },
+    pause: function(seconds) {
+      queue.splice(0, 0, {type: "pause", seconds: seconds});
+    }
+  };
+  
+  return self;
+}
