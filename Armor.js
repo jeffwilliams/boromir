6a8a2d83258b4a8ffb6ee086b570dac8d9859c6d
MaxMahem <MaxMahem@Win-Mahem>
2014-02-16 09:43:39 -0600

    tables tightened, started work on shields.

--- Armor.js
+++ Armor.js
@@ -31,3 +31,30 @@ Armor.makeArmor = function(name, type) {
 	  isUnique: false
     });
 };
+
+var Shield = new Equipment();
+
+Shield.SHIELDS = [
+//    name,                  GP, AC, DEX, ACP, Weight
+    [ "light wooden shield",  3,  1,    ,  -1,  5 ],
+    [ "light metal shield",   9,  1,    ,  -1,  6 ],
+    [ "heavy wooden shield",  7,  2,    ,  -2, 10 ],
+    [ "heavy metal shield",  20,  2,    ,  -2, 15 ],
+    [ "tower shield",        30,  4,   2, -10, 45 ],
+];
+
+Shield.NONE = [
+    [ "no shield",            0,  0,    ,   0,  0 ],
+]
+
+Shield.makeShield = function(name, type) {
+    type = typeof type !== 'undefined' ? type : Shield.SHIELDS;
+    
+    var equipmentSelected = this.selectEquipment(name, type)
+
+    return Combat.Shield({
+          name: equipmentSelected[0],
+	  armorBonus: equipmentSelected[2],
+	  isUnique: false
+    });
+};
--- Weapons.js
+++ Weapons.js
@@ -3,23 +3,23 @@ var Weapons = new Equipment();
 // Crit refers to crit range, distance from 20 you need to roll to crit. So
 // a crit range of 20 is 0, 19-20 is 1, 18-20 is 3, ect.
 // Mult refers to the multipler on a crit, x2, x3, ect..
-//     name,             GP,    Die, Crit, Mult, lbs, damage type
+//         name,          Die, Crit, Mult, damage,    type
 Weapons.LIGHTWEAPONS = [
-	[ "dagger",           2,  "1d4",    1,    2,   1, "pierce" ],
-	[ "handaxe",          6,  "1d6",    0,    3,   3, "slash" ],
-	[ "shortsword",      10,  "1d6",    1,    2,   2, "pierce" ],
+	[ "dagger",     "1d4",    1,    2, "pierce", "light" ],
+	[ "handaxe",    "1d6",    0,    3, "slash",  "light" ],
+	[ "shortsword", "1d6",    1,    2, "pierce", "light" ],
 ];
   
 Weapons.MEDIUMWEAPONS = [
-	[ "heavy mace",      12,  "1d8",    0,    2,   8, "bludgeon" ],
-	[ "warhammer",       12,  "1d8",    0,    3,   5, "bludgeon" ],
-	[ "longsword",       15,  "1d8",    1,    2,   4, "slash" ],
-	[ "battleaxe",       10,  "1d8",    0,    3,   7, "slash" ],      
+	[ "heavy mace", "1d8",    0,    2, "bludgeon", "1hand" ],
+	[ "warhammer",  "1d8",    0,    3, "bludgeon", "1hand" ],
+	[ "longsword",  "1d8",    1,    2, "slash",    "1hand" ],
+	[ "battleaxe",  "1d8",    0,    3, "slash" ],      
 ];
   
 Weapons.HEAVYWEAPONS = [
-        [ "greatsword",      50,  "2d6",    1,    2,  15, "slash" ],
-        [ "greataxe",        20, "1d12",    0,    3,  12, "slash" ],
+        [ "greatsword", "2d6",    1,    2, "slash",    "1hand" ],
+        [ "greataxe",  "1d12",    0,    3, "slash",    "1hand" ],
 ];
   
 Weapons.WEAPONS = Weapons.LIGHTWEAPONS.concat(Weapons.MEDIUMWEAPONS, Weapons.HEAVYWEAPONS)
@@ -31,10 +31,10 @@ Weapons.makeWeapon = function(name, type) {
     
     return Combat.Weapon({
         name: equipmentSelected[0],
-	damage: equipmentSelected[2],
+	damage: equipmentSelected[1],
 	isUnique: false,
-	critRange: equipmentSelected[3],
-	critMultiplier: equipmentSelected[4],
-        words: WORDS[equipmentSelected[6]]
+	critRange: equipmentSelected[2],
+	critMultiplier: equipmentSelected[3],
+        words: WORDS[equipmentSelected[4]]
     });
 };
